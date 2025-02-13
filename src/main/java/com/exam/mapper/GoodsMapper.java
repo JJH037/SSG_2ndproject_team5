415d313c891796e04b5225138b62fa59b50a7ca8
Jeong JaeHoon <jyuzu037@gmail.com>
2025-02-13 11:14:21 +0900

    02131114

--- src/main/java/com/exam/mapper/GoodsMapper.java
+++ src/main/java/com/exam/mapper/GoodsMapper.java
@@ -22,11 +22,5 @@ public interface GoodsMapper {
 
 	public void addGoods(GoodsDTO goodsDTO); // 상품 추가 메서드
 
-    // 모든 상품의 재고 정보 조회
-	public  List<GoodsDTO> getAllStock();
 	
-	  // 모든 상품의 재고 정보 조회
-		public  List<RefrigeratorDTO> getAllrStock();
-		public     void addGoods(GoodsDTO goodsDTO); // 상품 추가 메서드
-
 }
